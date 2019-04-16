a0b49e3b4f1bd4509369dc438404fbf526ad5944
yinsua <ausniy@gmail.com>
2019-04-16 12:26:42 +0800

    Update etherlab_master_install.sh

--- etherlab_master_install.sh
+++ etherlab_master_install.sh
@@ -24,3 +24,11 @@ echo DEVICE_MODULES=\"generic\" | sudo tee -a $ETHERLAB_PREFIX/etc/ethercat.conf
 sudo cp $ETHERLAB_PREFIX/etc/ethercat.conf $ETHERLAB_PREFIX/etc/sysconfig/ethercat &&
 sudo cp $ETHERLAB_PREFIX/etc/sysconfig/ethercat /etc/sysconfig &&
 sudo depmod 
+
+sudo modprobe ec_master &&
+sudo $ETHERLAB_PREFIX/etc/init.d/ethercat start &&
+echo KERNEL==\"EtherCAT[0-9]\", MODE=\"0777\" | sudo tee /etc/udev/rules.d/99-ethercat.rules &&
+sudo udevadm control --reload-rules &&
+echo $ETHERLAB_PREFIX/lib | sudo tee -a /etc/ld.so.conf &&
+sudo ldconfig &&
+echo `echo $USER` hard rtprio 99 | sudo tee -a /etc/security/limits.conf
