63b1de259cfc52be93d9b9527cc743e56d4a51c8
yinsua <ausniy@gmail.com>
2019-04-16 12:04:27 +0800

    Update etherlab_master_install.sh

--- etherlab_master_install.sh
+++ etherlab_master_install.sh
@@ -3,24 +3,24 @@ export KERNEL_VERSION="4.4.0-140-generic"
 export MASTER0_DEVICE="00:90:27:e0:f1:aa"
 export ETHERLAB_PREFIX="/opt/etherlab"
 
-set -ex \
-&& apt-get install -y build-essential \
-&& apt-get install -y linux-image-extra-$KERNEL_VERSION linux-headers-$KERNEL_VERSION \
-&& apt-get install -y mercurial autoconf autogen libtool make \
-&& export ETHERLAB_DIR="etherlabmaster-code" 
+set -ex &&
+sudo apt-get install -y build-essential &&
+sudo apt-get install -y linux-image-extra-$KERNEL_VERSION linux-headers-$KERNEL_VERSION &&
+sudo apt-get install -y mercurial autoconf autogen libtool make &&
+export ETHERLAB_DIR="etherlabmaster-code" 
 if [ ! -d $ETHERLAB_DIR ]; then
     hg clone http://hg.code.sf.net/p/etherlabmaster/code "$ETHERLAB_DIR" -r stable-1.5 
 fi
-cd $ETHERLAB_DIR \
-&& ./bootstrap \
-&& ./configure --prefix=$ETHERLAB_PREFIX --with-linux-dir=/usr/src/linux-headers-$KERNEL_VERSION --enable-cycles --enable-hrtimer --enable-8139too=no --enable-r8169=yes --enable-eoe=no \
-&& make -j$(nproc) \
-&& make all modules \
-&& make install \
-&& make modules_install \
-&& mkdir -p /etc/sysconfig \
-&& echo MASTER0_DEVICE=\"$MASTER0_DEVICE_MAC\" > $ETHERLAB_PREFIX/etc/ethercat.conf \
-&& echo DEVICE_MODULES=\"generic\" >> $ETHERLAB_PREFIX/etc/ethercat.conf \
-&& cp $ETHERLAB_PREFIX/etc/ethercat.conf $ETHERLAB_PREFIX/etc/sysconfig/ethercat \
-&& cp $ETHERLAB_PREFIX/etc/sysconfig/ethercat /etc/sysconfig \
-&& depmod 
+cd $ETHERLAB_DIR 
+./bootstrap &&
+./configure --prefix=$ETHERLAB_PREFIX --with-linux-dir=/usr/src/linux-headers-$KERNEL_VERSION --enable-cycles --enable-hrtimer --enable-8139too=no --enable-r8169=yes --enable-eoe=no &&
+make -j$(nproc) &&
+make all modules &&
+sudo make install &&
+sudo make modules_install &&
+sudo mkdir -p /etc/sysconfig &&
+echo MASTER0_DEVICE=\"$MASTER0_DEVICE_MAC\" > $ETHERLAB_PREFIX/etc/ethercat.conf &&
+echo DEVICE_MODULES=\"generic\" >> $ETHERLAB_PREFIX/etc/ethercat.conf &&
+cp $ETHERLAB_PREFIX/etc/ethercat.conf $ETHERLAB_PREFIX/etc/sysconfig/ethercat &&
+sudo cp $ETHERLAB_PREFIX/etc/sysconfig/ethercat /etc/sysconfig &&
+sudo depmod 
